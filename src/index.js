2d31728885cb0641ed593a9d67eb3847ad6ede84
Tony Jacomelli <tjacomelli@gmail.com>
2019-10-29 17:57:16 +0100

    Initial Commit

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const path = require('path');
+import Prismic from 'prismic-javascript'
 
 function install(moduleOptions) {
   const options = {
@@ -6,27 +7,28 @@ function install(moduleOptions) {
     ...moduleOptions,
   };
 
+  
   // Add in Prismic libraries to enable preview
   if (typeof (this.options.head.__dangerouslyDisableSanitizersByTagID) === 'undefined') {
     this.options.head.__dangerouslyDisableSanitizersByTagID = {};
   }
-
+  
   if (typeof (this.options.head.script) === 'undefined') {
     this.options.head.script = [];
   }
-
+  
   this.options.head.__dangerouslyDisableSanitizersByTagID['prismic-nuxt'] = ['innerHTML'];
   this.options.head.script.push({
     hid: 'prismic-nuxt',
     innerHTML: `window.prismic = {endpoint: '${options.endpoint}'};`,
     type: 'text/javascript',
   });
-
+  
   this.options.head.script.push({
     src: '//static.cdn.prismic.io/prismic.min.js',
     ...(options.deferLoad && { defer: true }),
   });
-
+  
   // Add the plugin
   this.addPlugin({
     src: path.resolve(__dirname, 'plugin.js'),
@@ -37,6 +39,18 @@ function install(moduleOptions) {
     },
   });
 }
+
+// Use link resolver to generate Prismic based routes
+this.nuxt.hook('generate:before', async (generator) => {
+  const maybeF = this.options.generate.routes || []
+  this.options.generate.routes = async () => {
+    const client = await Prismic.client(options.endpoint);
+    const response = await client.query('');
+    const prismicRoutes = (response.results || []).map(options.linkResolver);
+    const userRoutes = typeof maybeF === 'function' ? await maybeF() : maybeF;
+    return prismicRoutes.concat(userRoutes);
+  };
+});
 
 module.exports = install;
 module.exports.meta = require('../package.json');
