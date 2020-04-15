786ebff82bcb786abbb031230fd79515d01b9137
Arnaud Lewis <arnaud.lewis@gmail.com>
2020-04-15 16:51:33 +0200

    return null by default in the link resolver to be able to fallback on the new one

--- src/templates/link-resolver.js
+++ src/templates/link-resolver.js
@@ -1,2 +1,2 @@
 <% if (typeof options.linkResolver === 'string') { %>export { default } from '<%= options.linkResolver %>'<% }
-else { %>export default <%= options.linkResolver ? serialize(options.linkResolver) : "function (doc) { return '/' }" %><% } %>
+else { %>export default <%= options.linkResolver ? serialize(options.linkResolver) : "function (doc) { return null }" %><% } %>
