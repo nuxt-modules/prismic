2c793e49d6450030207c4b844bd24cfac05e474f
Arnaud Lewis <arnaud.lewis@gmail.com>
2020-04-17 18:19:04 +0200

    change dependency for prismic-vue

--- src/templates/plugins/prismic-components.js
+++ src/templates/plugins/prismic-components.js
@@ -1,5 +1,5 @@
 import Vue from 'vue'
-import { common, nuxt } from 'prismic-vue/components/common'
+import { common, nuxt } from '@prismicio/vue/components/common'
 
 Object.entries({ ...common, ...nuxt }).forEach(([_, c]) => {
   Vue.component(c.name, c)
