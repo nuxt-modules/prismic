a9c4bf8be1306a0f27f23bc61431ef5b826bbbe1
lihbr <lihbr@users.noreply.github.com>
2020-08-01 14:45:47 +0200

    docs: re-add google analytics

--- docs/nuxt.config.js
+++ docs/nuxt.config.js
@@ -2,13 +2,22 @@ import path from 'path';
 import theme from '@nuxt/content-theme-docs';
 
 export default theme({
-  buildModules: (buildModules) => buildModules.map((module) => {
-    if (module !== '@nuxtjs/tailwindcss') {
-      return module;
-    }
+  generate: {
+    fallback: true,
+    routes: ['/'],
+  },
+  buildModules: (buildModules) => [
+    ...buildModules.map((module) => {
+      if (module !== '@nuxtjs/tailwindcss') {
+        return module;
+      }
 
-    return ['@nuxtjs/tailwindcss', {
-      configPath: path.resolve(__dirname, 'tailwind.config.js'),
-    }];
-  }),
+      return ['@nuxtjs/tailwindcss', {
+        configPath: path.resolve(__dirname, 'tailwind.config.js'),
+      }];
+    }),
+    ['@nuxtjs/google-analytics', {
+      id: 'UA-27914050-11'
+    }]
+  ],
 });
