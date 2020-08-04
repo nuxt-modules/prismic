d240a351474e7134c6336667628ba1f96bc25aef
lihbr <lihbr@users.noreply.github.com>
2020-08-04 11:48:26 +0200

    docs: use prismic shades instead of default blue

--- docs/tailwind.config.js
+++ docs/tailwind.config.js
@@ -1,15 +1,20 @@
 const defu = require('defu');
 const contentTailwind = require('@nuxt/content-theme-docs/src/tailwind.config.js');
-const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = defu({
   theme: {
     extend: {
       colors: {
         primary: {
-          ...defaultTheme.colors.blue,
+          100: '#F2F4FA',
+          200: '#DFE2F2',
+          300: '#CCD1EB',
+          400: '#A5AFDB',
           500: '#7F8CCC',
-          900: '#243956'
+          600: '#727EB8',
+          700: '#4C547A',
+          800: '#393F5C',
+          900: '#262A3D'
         },
       },
     },
