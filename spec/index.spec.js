6f60b21d596306585391ffde7a662b1a87fdbf11
Lucie <25330882+lihbr@users.noreply.github.com>
2021-03-09 14:06:39 +0100

    fix: preview not persisting after navigation (#129)
    * fix: preview not persisting after navigation
    
    * test: update test
    
    Co-authored-by: lihbr <lihbr@users.noreply.github.com>

--- spec/index.spec.js
+++ spec/index.spec.js
@@ -60,26 +60,23 @@ describe('prismic-nuxt module', () => {
 
   it('should add the components', () => {
     prismicNuxt.call(context, moduleOptions);
-    expect(context.addPlugin.mock.calls).toHaveLength(3);
+    expect(context.addPlugin.mock.calls).toHaveLength(2);
   });
 
   it('should not add the components if components=false', () => {
     prismicNuxt.call(context, { ...moduleOptions, components: false });
-    expect(context.addPlugin.mock.calls).toHaveLength(2);
+    expect(context.addPlugin.mock.calls).toHaveLength(1);
   });
 
   it('should set preview to /preview if true', () => {
     prismicNuxt.call(context, { ...moduleOptions, preview: true });
-    expect(context.addPlugin.mock.calls.length).toEqual(3);
-    expect(context.addPlugin.mock.calls[0][0].fileName).toEqual('prismic/middleware/prismic_preview.js');
-    expect(context.addPlugin.mock.calls[2][0].options.preview).toEqual('/preview');
-    expect(context.options.router.middleware).toEqual(['prismic_preview']);
+    expect(context.addPlugin.mock.calls.length).toEqual(2);
+    expect(context.addPlugin.mock.calls[1][0].options.preview).toEqual('/preview');
   });
 
   it('should set preview to /test_preview', () => {
     prismicNuxt.call(context, { ...moduleOptions, preview: '/test_preview' });
-    expect(context.addPlugin.mock.calls[0][0].fileName).toEqual('prismic/middleware/prismic_preview.js');
-    expect(context.addPlugin.mock.calls[2][0].options.preview).toEqual('/test_preview');
+    expect(context.addPlugin.mock.calls[1][0].options.preview).toEqual('/test_preview');
     expect(context._routes[0].path).toEqual('/test_preview');
     expect(context._resolve.mock.calls[0]).toEqual(['/var/nuxt/.nuxt/', 'prismic/pages/preview.vue']);
   });
@@ -91,7 +88,7 @@ describe('prismic-nuxt module', () => {
 
   it('should parse repo from endpoint', () => {
     prismicNuxt.call(context, { endpoint: 'https://test2.prismic.io/api/v2' });
-    expect(context.addPlugin.mock.calls[2][0].options.repo).toEqual('test2');
+    expect(context.addPlugin.mock.calls[1][0].options.repo).toEqual('test2');
   });
 
   it('should warn to create ~/app/prismic/link-resolver.js', async () => {
