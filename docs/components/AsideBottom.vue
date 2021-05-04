22cfb5d57013f2dea4cb09be7e0a9942105de705
lihbr <lihbr@users.noreply.github.com>
2021-05-04 16:58:58 +0200

    docs: update docus

---
+++ docs/components/AsideBottom.vue
@@ -0,0 +1,80 @@
+<template>
+  <div>
+    <h5
+      class="py-2 text-base font-semibold text-gray-900 transition duration-200 cursor-pointer dark:text-gray-100"
+    >
+      Resources
+    </h5>
+    <ul>
+      <li>
+        <a
+          href="https://github.com/nuxt-community/prismic-module/blob/master/CHANGELOG.md"
+          target="_blank"
+          rel="noopener"
+          class="flex items-center transition duration-200 py-2 group dark:hover:text-gray-100 hover:text-gray-900"
+        >
+          <span>✨ &nbsp;Release Notes</span>
+          <svg
+            fill="none"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            class="w-4 h-4 ml-2 text-gray-400 opacity-0 group-hover:opacity-100"
+          >
+            <path
+              d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
+            />
+          </svg>
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://community.prismic.io"
+          target="_blank"
+          rel="noopener"
+          class="flex items-center transition duration-200 py-2 group dark:hover:text-gray-100 hover:text-gray-900"
+        >
+          <span>📣 &nbsp;Community Forum</span>
+          <svg
+            fill="none"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            class="w-4 h-4 ml-2 text-gray-400 opacity-0 group-hover:opacity-100"
+          >
+            <path
+              d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
+            />
+          </svg>
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://prismic.io/docs?utm_campaign=devexp&utm_source=nuxt2doc&utm_medium=doc"
+          target="_blank"
+          rel="noopener"
+          class="flex items-center transition duration-200 py-2 group dark:hover:text-gray-100 hover:text-gray-900"
+        >
+          <span>📚 &nbsp;Official Documentation</span>
+          <svg
+            fill="none"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            class="w-4 h-4 ml-2 text-gray-400 opacity-0 group-hover:opacity-100"
+          >
+            <path
+              d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
+            />
+          </svg>
+        </a>
+      </li>
+    </ul>
+  </div>
+</template>
--- docs/nuxt.config.js
+++ docs/nuxt.config.js
@@ -1,18 +1,15 @@
 import { withDocus } from 'docus'
 
 export default withDocus({
-  docus: {
-    colors: {
-      primary: '#7F8CCC'
-    }
-  },
   generate: {
     fallback: true
   },
   buildModules: [
-    'vue-plausible'
-  ],
-  plausible: {
-    domain: 'prismic.nuxtjs.org'
-  }
+    [
+      'vue-plausible',
+      {
+        domain: 'prismic.nuxtjs.org'
+      }
+    ]
+  ]
 })
