18f6d263d715b385bbd285f602e0f7b53e086917
lihbr <lihbr@users.noreply.github.com>
2021-12-06 13:14:19 +0100

    docs: remove doc

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -2,5 +2,5 @@ import { defineNuxtConfig } from "nuxt3";
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
-	buildModules: ["../"],
+	// buildModules: ["../"],
 });
