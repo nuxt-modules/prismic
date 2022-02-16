c010a55a8985da32cfce50d1b01d7a1cf308cf44
lihbr <lihbr@users.noreply.github.com>
2022-02-16 17:09:43 +0100

    docs: advertise Nuxt 3 version

--- docs/components/AsideBottom.vue
+++ docs/components/AsideBottom.vue
@@ -75,6 +75,27 @@
           </svg>
         </a>
       </li>
+      <li>
+        <a
+          href="https://v3.prismic.nuxtjs.org"
+          class="flex items-center transition duration-200 py-2 group dark:hover:text-gray-100 hover:text-gray-900"
+        >
+          <span>🚀 &nbsp;Nuxt 3 version</span>
+          <svg
+            fill="none"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            class="w-4 h-4 ml-2 text-gray-400 opacity-0 group-hover:opacity-100"
+          >
+            <path
+              d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
+            />
+          </svg>
+        </a>
+      </li>
     </ul>
   </div>
 </template>
