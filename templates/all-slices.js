766f39c5af43e4ffce11a37aaf2671928bd3a444
lihbr <lihbr@users.noreply.github.com>
2023-03-13 17:32:51 +0100

    fix: remove lodash dependency on templates

--- templates/all-slices.js
+++ templates/all-slices.js
@@ -1,7 +1,7 @@
-<% options.libraries.forEach((library, index) => { %>import { components as library<%- index %> } from '<%- library %>'
+<% options.libraries.forEach((library, index) => { %>import { components as library<%= index %> } from '<%= library %>'
 <% }); %>
 
 export default {
-<% options.libraries.forEach((library, index) => { %>  ...library<%- index %>,
+<% options.libraries.forEach((library, index) => { %>  ...library<%= index %>,
 <% }); %>
 }
