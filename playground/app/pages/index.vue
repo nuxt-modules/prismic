6c554671e5490f71cb1edffb70b52159d75dad3e
lihbr <lihbr@users.noreply.github.com>
2025-01-02 18:26:45 +0100

    chore: cleanup playground

--- playground/app/pages/index.vue
+++ playground/app/pages/index.vue
@@ -4,18 +4,11 @@ import { usePrismic, useAsyncData, ref } from '#imports'
 const prismic = usePrismic()
 
 const { data: doc } = await useAsyncData('doc', () => prismic.client.getSingle('kitchen_sink'))
-
-const link = ref({ link_type: 'Web', key: 'ad8a212f-4faa-42d0-80dc-6a8495205000', url: 'https://awwwards.com/jankohlbach', target: '_blank', text: 'Awwwards' })
 </script>
 
 <template>
 	<div>
 		{{ doc?.id }} - {{ doc?.type }} - {{ doc?.last_publication_date }}
-		<PrismicLink :field="link" />
-		<PrismicLink :field="link" />
-		<PrismicLink :field="link">
-			hello
-		</PrismicLink>
 		<span
 			:class="null"
 			:data-foo="null"
